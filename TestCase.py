60d4d8c4c06c32378715bd0bd9ec7cbb5cb8800a
60050211 <43135341+Praeee@users.noreply.github.com>
2021-05-23 22:40:54 +0700

    test case

---
+++ TestCase.py
@@ -0,0 +1,33 @@
+from selenium.webdriver import Chrome
+
+path = "C:\\Users\\paer_\chromedriver.exe"
+driver = Chrome(executable_path=path)
+driver.get("http://the-internet.herokuapp.com/login")
+
+# Maximum browser
+driver.maximize_window()
+
+
+def test(x, y):
+    #   Text Username,Password
+    driver.find_element_by_name("username").send_keys(x)
+    driver.find_element_by_name("password").send_keys(y)
+    #   Button Submit
+    driver.find_element_by_xpath("//button[@type='submit']").click()
+
+
+def logout():
+    #   Button Logout
+    driver.find_element_by_xpath('/html/body/div[2]/div/div/a').click()
+
+
+# testcase : 1
+test("tomsmith", "SuperSecretPassword!")
+logout()
+
+
+test("tomsmith", "Password!")
+logout()
+
+test("tomholland", "Password!")
+logout()
